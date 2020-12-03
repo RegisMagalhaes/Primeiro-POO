cfa9acb2ffd7280407638a8cc785f93b42321e26
Regis Leandro da Silva Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-12-03 14:39:25 -0300

    Meu primeiro orientado a objetos

---
+++ Personagem.cs
@@ -0,0 +1,28 @@
+namespace POO
+{
+    public class Personagem
+    {
+        public string nome;
+
+        public int idade;
+
+        public string armadura;
+
+        public string ia;
+
+        public string Ataque1(){
+
+            return "Ataque Voador";
+        }
+
+        public string Ataque2(){
+
+            return "Ataque feixe de luz";
+        }
+
+        public string Defender(){
+
+            return "Não foi dessa vez";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciando objeto
+            Personagem jogador1 = new Personagem();
+            jogador1.nome = "Regis";
+            jogador1.idade = 36;
+            jogador1.armadura = "faceshield";
+            jogador1.ia = "Jarvis";
+
+            Personagem jogador2 = new Personagem();
+            jogador2.nome = "Denis";
+            jogador2.idade = 33;
+            jogador2.armadura = "bota";
+            jogador2.ia = "Verônica";
+
+            Console.WriteLine($"Partida: {jogador1.nome} vs {jogador2.nome}");
+            Console.WriteLine($"Jogador 1 Armadura: {jogador1.armadura} || Jogador 2 Armadura: {jogador2.armadura}");
+            Console.WriteLine($"Jogador 1 IA: {jogador1.ia} || Jogador 2 IA: {jogador2.ia}");
+        }
+    }
+}
